491df392d1e440c8995673f029a02315c4d63f01
Gabe G'Sell <gabergg@gmail.com>
2014-08-23 23:51:21 -0700

    basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeAndBeer::Application.config.secret_key_base = '9970efeac384f887222373f74a7a0b1e3b5946e0527ff1a3f8175cdfab5b81965c50a4c0a5d56a5708fca705db3b7b2d1076102a27ffbb2fee557483cd2af0b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoffeeAndBeer::Application.config.session_store :cookie_store, key: '_coffee_and_beer_session'
