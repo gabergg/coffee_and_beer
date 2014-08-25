a1dd201ca23d7a5b73f3bd6552af4a85cea4a47b
Gabe G'Sell <gabergg@gmail.com>
2014-08-24 23:22:34 -0700

    map cleanup

--- app/assets/javascripts/meetings_map.js
+++ app/assets/javascripts/meetings_map.js
@@ -13,13 +13,13 @@ function initialize_map() {
     var map = new google.maps.Map(document.getElementById("map-canvas"), options);
     var html = "<table>" +
         "<tr><td>Name:</td> <td><input type='text' id='name'/> </td> </tr>" +
-        "<tr><td>Address:</td> <td><input type='text' id='address'/></td> </tr>" +
         "<tr><td>Phone: </td><td><input type='text' id='phone'/></td></tr>" +
-        "<tr><td>Beverage:</td> <td><select id='beverage'>" +
+        "<tr><td>Activity:</td> <td><select id='beverage'>" +
         "<option value='bar' SELECTED>Coffee</option>" +
         "<option value='restaurant'>Beer</option>" +
+        "<option value='other'>Other</option>" +
         "</select> </td></tr>" +
-        "<tr><td></td><td><input type='button' value='Save & Close' onclick='saveData()'/></td></tr>";
+        "<tr><td></td><td><input type='button' value='Post' onclick='saveData()'/></td></tr>";
     infowindow = new google.maps.InfoWindow({
         content: html
     });
@@ -87,39 +87,6 @@ function loadScript() {
     document.body.appendChild(script);
 }
 
-
 function saveData() {
-    var name = escape(document.getElementById("name").value);
-    var address = escape(document.getElementById("address").value);
-    var phone = escape(document.getElementById("phone").value);
-    var beverage = document.getElementById("beverage").value;
-    var latlng = marker.getPosition();
 
-    var url = "phpsqlinfo_addrow.php?name=" + name + "&address=" + address +
-        "&type=" + type + "&lat=" + latlng.lat() + "&lng=" + latlng.lng();
-    downloadUrl(url, function (data, responseCode) {
-        if (responseCode == 200 && data.length >= 1) {
-            infowindow.close();
-            document.getElementById("message").innerHTML = "Location added.";
-        }
-    });
-}
-
-function downloadUrl(url, callback) {
-    var request = window.ActiveXObject ?
-        new ActiveXObject('Microsoft.XMLHTTP') :
-        new XMLHttpRequest;
-
-    request.onreadystatechange = function () {
-        if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request.responseText, request.status);
-        }
-    };
-
-    request.open('GET', url, true);
-    request.send(null);
-}
-
-function doNothing() {
 }
