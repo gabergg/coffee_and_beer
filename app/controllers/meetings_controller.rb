e39021e82bc53ba9ce23439f2415dcec4e83a202
Gabe G'Sell <gabergg@gmail.com>
2014-08-26 01:52:54 -0700

    map updates

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,9 +1,19 @@
 class MeetingsController < ApplicationController
 
   def create
-    @meeting = Meeting.new
-    flash[:notice] = params[:data]
+    @meeting = Meeting.new(email: params[:email], name: params[:name], phone: params[:phone],
+                           meeting_type: params[:meeting_type],
+                           latitude: params[:latitude].to_f, longitude: params[:longitude].to_f).save
     redirect_to root_path
   end
+
+=begin
+  t.string   "email"
+  t.string   "meeting_type"
+  t.string   "name"
+  t.string   "phone"
+  t.float    "latitude"
+  t.float    "longitude"
+=end
 
 end
