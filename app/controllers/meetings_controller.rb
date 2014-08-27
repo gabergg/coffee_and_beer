e3f1678655213ce8bfec03186f77ec8e3f0a6748
Gabe G'Sell <gabergg@gmail.com>
2014-08-26 19:49:24 -0700

    all markers kept from db

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -3,7 +3,13 @@ class MeetingsController < ApplicationController
   def create
     @meeting = Meeting.new(email: params[:email], name: params[:name], phone: params[:phone],
                            meeting_type: params[:meeting_type],
-                           latitude: params[:latitude].to_f, longitude: params[:longitude].to_f).save
+                           latitude: params[:latitude].to_f, longitude: params[:longitude].to_f)
+    begin
+      @meeting.save
+    rescue => e
+      logger.warn "Failed to save new meeting for email: " + params[:email] + " and phone " + params[:phone]
+    end
+
     redirect_to root_path
   end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    gon.markers = Meeting.all.select("latitude, longitude")
   end
 
   def blog
