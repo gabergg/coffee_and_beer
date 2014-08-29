7147b37d469989b252b17fa76a477114963736a0
Gabe G'Sell <gabergg@gmail.com>
2014-08-28 20:38:14 -0700

    working errors within info window

--- app/assets/javascripts/validations.js
+++ app/assets/javascripts/validations.js
@@ -8,23 +8,44 @@ function validateForm() {
     hasPhone = true;
     validEmail = false;
     validPhone = false;
-    var reason = "";
+    var reasons = [];
 
-    reason += validateEmail($('#email'));
-    reason += validatePhone($('#phone'));
+    reasons.push(validateEmail($('#email')));
+    reasons.push(validatePhone($('#phone')));
 
-    if(!(hasEmail || hasPhone)) {
+    if (!(hasEmail || hasPhone)) {
         $('#email').addClass('invalid-input');
         $('#phone').addClass('invalid-input');
-        reason += "We need either your phone or your email!";
+        reasons.push("We need either your phone or your email!");
     }
 
-    if (reason != "" && !(validEmail || validPhone)) {
-        alert("Your form looks great! Except...\n" + reason);
+    if (reasons != "" && !(validEmail || validPhone)) {
+        html_error = "<div class='alert alert-error'>" +
+            "<a href='#' class='close' data-dismiss='alert'>&times;</a>" +
+            "<strong>Whoops!</strong> Your form looks great! Except..." +
+            error_set(reasons) +
+            "</div>"
+
+        $('#home_content').prepend(html_error);
         return false;
     }
 
+    html_success = "<div class='alert alert-success'>" +
+        "<a href='#' class='close' data-dismiss='alert'>&times;</a>" +
+        "<strong>Success!</strong> You have been added to our trip!" +
+        "</div>"
+
+    $('#home_content').prepend(html_success);
+
     return true;
+}
+
+function error_set(reasons) {
+    var error_html = "";
+    for (var i = 0; i < reasons.length; i++) {
+        error_html += "<p>" + reasons[i] + "</p>";
+    }
+    return error_html;
 }
 
 function trim(s) {
@@ -45,11 +66,11 @@ function validateEmail(fld) {
     } else if (!emailFilter.test(tfld)) {              //test email for illegal characters
         fld.addClass('invalid-input');
         console.log("2");
-        error = "That's not an email address!\n";
+        error = "That's not an email address!";
     } else if (value.match(illegalChars)) {
         fld.addClass('invalid-input');
         console.log("3");
-        error = "Your email address has some illegal characters!\n";
+        error = "Your email address has some illegal characters!";
     } else {
         validEmail = true;
         fld.removeClass('invalid-input');
@@ -65,10 +86,10 @@ function validatePhone(fld) {
     if (value == "") {
         hasPhone = false;
     } else if (isNaN(parseInt(stripped))) {
-        error = "Your phone number has some illegal characters!\n";
+        error = "Your phone number has some illegal characters!";
         fld.addClass('invalid-input');
     } else if (!(stripped.length == 10)) {
-        error = "The phone number isn't the right length! Did you include your area code?\n";
+        error = "The phone number isn't the right length! Did you include your area code?";
         fld.addClass('invalid-input');
     }
     else {
